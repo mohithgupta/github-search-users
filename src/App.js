1544717d24f40b01a6d808105af3cdae90392f80
You Know Who <you@example.commohithguptak@gmail.com>
2021-03-29 09:36:05 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { TextField } from "@material-ui/core";
+import { useEffect,useState } from "react";
+import "./App.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function App() {
+  const [username, setUsername] = useState("mohithgupta");
+  const [data, setData] = useState("");
+  const [login, setLogin] = useState("");
+
+  const url = "https://api.github.com/users/";
+
+  const search = (e) => {
+    if (e.keyCode === 13) {
+      setLogin(username);
+  }
+};
+
+useEffect(()=>{
+  fetch(url + username)
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    setData(data);
+    console.log(data);
+  });
+
+},[login])
+
+if(data){
+  document.getElementById("link").setAttribute("href", `${data.html_url}`)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex">
+      <div className="modal">
+        <TextField
+          label="User Name"
+          style={{ width: "100%" }}
+          variant="outlined"
+          value={username}
+          onKeyDown={(e) => search(e)}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="profile">
+        <div className="flex main">
+          <img src={data.avatar_url} alt="" />
+          <div className="main__right">
+            <h3>{data.name}</h3>
+            <div className="flex github-username">
+              <GitHubIcon style={{ marginRight: "5px" }} />
+              <p style={{ color: "#333" }}>{data.login}</p>
+            </div>
+            <a href="https://github.com/mohithgupta" target="_blank" id="link">Visit Profile</a>
+          </div>
+        </div>
+        <p>{data.bio}</p>
+        <div className="flex counters">
+          <div className="count">
+            <h3>{data.public_repos}</h3>
+            <p>Public Repos</p>
+          </div>
+          <div className="count">
+            <h3>{data.followers}</h3>
+            <p>Followers</p>
+          </div>
+          <div className="count">
+            <h3>{data.following}</h3>
+            <p>Following</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
