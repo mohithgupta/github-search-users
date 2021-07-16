cb725ef827caec2c054cc37e16ba601bcbf0e796
You Know Who <you@example.commohithguptak@gmail.com>
2021-07-16 13:11:21 +0530

    Updated the code to use setlogin for updating URL so that username is not set everytime we type, as requests are very limited

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 function App() {
   const [username, setUsername] = useState("mohithgupta");
   const [data, setData] = useState("");
-  const [login, setLogin] = useState("");
+  const [login, setLogin] = useState("mohithgupta");
 
   const url = "https://api.github.com/users/";
 
-  const search = (e) => {
-    if (e.keyCode === 13) {
-      setLogin(username);
-  }
-};
+  // For unauthenticated requests, the rate limit allows for up to 60 requests per hour.
+  // Unauthenticated requests are associated with the originating IP address, and not the user making requests.
+   
+  // I have not use Github Token or any authentication.
+  // So I updated the code to use setlogin to not set uername each time we type a character as requests are very limited.
 
 useEffect(()=>{
-  fetch(url + username)
+  fetch(url + login)
   .then((data) => {
     return data.json();
   })
@@ -25,7 +25,17 @@ useEffect(()=>{
     setData(data);
     console.log(data);
   });
-},[username,login])
+},[login])  
+
+// with live changing username and async based
+// useEffect(()=>{
+//   fetch(url + username)
+//   .then( async (data) => {
+//     data=await data.json();
+//     setData(data);
+//     console.log(data);
+//   });
+// },[username]) 
 
 if(data){
   document.getElementById("link").setAttribute("href", `${data.html_url}`)
@@ -38,24 +48,24 @@ if(data){
           label="User Name"
           style={{ width: "100%" }}
           variant="outlined"
-          value={username}
-          onKeyDown={(e) => search(e)}
+          // value={username}
+          onKeyDown={(e) => e.key==='Enter' && setLogin(username)}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
       <div className="profile">
         <div align="center" className="flex main">
-          <img src={data.avatar_url} alt={data.name ? data.name+" 's pic" : "User Not Found"}/>
+          <img src={data.avatar_url} alt={data.avatar_url ? data.name+"'s pic" : "Pic not Found"}/>
           <div className="main__right">
-            <h3>{data.name ? data.name : "User Not Found"}</h3>
+            <h3>{data.name ? data.name : "Name not Found"}</h3>
             <div className="flex github-username">
               <GitHubIcon style={{ marginRight: "5px" }} />
-              <p style={{ color: "#333" }}>{data.login}</p>
+              <p style={{ color: "#333" }}>{data.login ? data.login : "UserName Invalid"}</p>
             </div>
-            <a href="https://github.com/mohithgupta" target="_blank" rel="noreferrer" id="link">{data.name ? "Visit Profile" : "Back to HomePage"}</a>
+            <a href="https://github.com/mohithgupta" target="_blank" rel="noreferrer" id="link">{data.login ? "Visit Profile" : "Back to HomePage"}</a>
           </div>
         </div>
-        <p>{data.bio}</p>
+        <p>About : {data.bio ? data.bio : "Not Available"}</p>
         <div className="flex counters">
           <div className="count">
             <h3>{data.public_repos}</h3>
